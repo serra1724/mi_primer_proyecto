3d525640ee5bd4862e0b3f8ec781b3f9ce3e6881
Juan David Serrano Marchena <judasema17@gmail.com>
2025-12-03 19:56:38 -0500

    Segunda versión: se muestra la longitud del nombre

--- hola.py
+++ hola.py
@@ -1,5 +1,5 @@
 # hola.py
-# Autor: (tu nombre)
-# Descripción: Script básico que saluda al usuario
+# Versión mejorada
 nombre = input("¿Cómo te llamas? ")
 print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
+print(f"Tu nombre tiene {len(nombre)} letras.")
